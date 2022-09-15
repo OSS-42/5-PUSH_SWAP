ec6084f5dd0bc9e925863f8ef93d3b785ccda1f8
xDeadpoolx <eric.wursteisen@gmail.com>
2022-09-14 20:13:37 -0400

    update push_swap starting bonus

---
+++ bonus/checker_bonus.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 19:52:48 by ewurstei          #+#    #+#             */
+/*   Updated: 2022/09/14 19:52:48 by ewurstei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_BONUS_H
+# define CHECKER_BONUS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "includes/libft/libft.h"
+
+#endif
